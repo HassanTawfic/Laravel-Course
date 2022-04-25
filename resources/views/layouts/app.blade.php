2f3fdbf4d8e516e75a3afbdc4bb4fe7f72bfc37e
HassanTawfic <hatawfic@gmail.com>
2022-04-25 02:54:02 +0200

    added middleware authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -11,16 +11,19 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+    <div class="bg-dark h-100" id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -66,7 +69,7 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
                                         @csrf
                                     </form>
                                 </div>
@@ -77,11 +80,11 @@
             </div>
         </nav>
 
-        <main class="py-4 bg-dark">
+        <main class="container py-4 bg-dark">
             @yield('content')
         </main>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',[BlogController::class,'index'])->name('blogs.index');
-Route::get('/blog', [BlogController::class,'index']);
-Route::get('/blog/create',[BlogController::class, 'create']);
-Route::post('/blog/store',[BlogController::class,'store'])->name('blogs.store');
-Route::put('/blog/{id}',[BlogController::class,'update'])->name('blogs.update');
-Route::get('/blog/{id}',[BlogController::class,'show'])->name('blogs.show');
-Route::get('/blog/{id}/edit',[BlogController::class,'edit'])->name('blogs.edit');
-Route::delete('/blog/{id}',[BlogController::class,'destroy'])->name('blogs.destroy');
-Route::get('/comment/{id}',[CommentController::class,'show'])->name('comments.show');
-Route::get('/blog/{blogId}/comments/create',[CommentController::class,'create'])->name('blogs.comments.create');
-Route::post('/blogs/{blogId}/comments',[CommentController::class,'store'])->name('blogs.comments.store');
-Route::get('/comments/{commentId}/edit',[CommentController::class,'edit'])->name('comments.edit');
-Route::put('/comments/{commentId}',[CommentController::class,'update'])->name('comments.update');
-Route::delete('/comment/{commentId}',[CommentController::class,'destroy'])->name('comments.destroy');
+Route::get('/',[BlogController::class,'index'])->name('blogs.index')->middleware('auth');
+Route::get('/blog', [BlogController::class,'index'])->middleware('auth');
+Route::get('/blog/create',[BlogController::class, 'create'])->middleware('auth');
+Route::post('/blog/store',[BlogController::class,'store'])->name('blogs.store')->middleware('auth');
+Route::put('/blog/{id}',[BlogController::class,'update'])->name('blogs.update')->middleware('auth');
+Route::get('/blog/{id}',[BlogController::class,'show'])->name('blogs.show')->middleware('auth');
+Route::get('/blog/{id}/edit',[BlogController::class,'edit'])->name('blogs.edit')->middleware('auth');
+Route::delete('/blog/{id}',[BlogController::class,'destroy'])->name('blogs.destroy')->middleware('auth');
+Route::get('/comment/{id}',[CommentController::class,'show'])->name('comments.show')->middleware('auth');
+Route::get('/blog/{blogId}/comments/create',[CommentController::class,'create'])->name('blogs.comments.create')->middleware('auth');
+Route::post('/blogs/{blogId}/comments',[CommentController::class,'store'])->name('blogs.comments.store')->middleware('auth');
+Route::get('/comments/{commentId}/edit',[CommentController::class,'edit'])->name('comments.edit')->middleware('auth');
+Route::put('/comments/{commentId}',[CommentController::class,'update'])->name('comments.update')->middleware('auth');
+Route::delete('/comment/{commentId}',[CommentController::class,'destroy'])->name('comments.destroy')->middleware('auth');
 
 Auth::routes();
 
