9b391ee443aca896f0cffc0dd29bdd89ca8f8afb
HassanTawfic <hatawfic@gmail.com>
2022-04-30 03:44:53 +0200

    scheduling tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\PruneOldPostsJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function (){
+        PruneOldPostsJob::class->handle();
+        })->daily();
     }
 
     /**
